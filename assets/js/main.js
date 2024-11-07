67d4c015d7505b84c0bab80307b8a0dff65b9771
Ramin Khalouie <ra.khalouie@gmail.com>
2024-11-07 14:04:20 +0330

    Complete Header section
    complete header section create and responsive
    fix some bugs in main navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // nav menu
 const toggleMenu = document.querySelector(".toggle-menu");
-const menu = document.querySelector(".main-nav>.mobile-menu");
+const menu = document.querySelector(".main-nav .mobile-menu");
 toggleMenu.addEventListener('click', () => {
     toggleMenu.classList.toggle('show');
     menu.classList.toggle('show');
